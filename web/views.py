c326df31f08494e06a372aa6be56ee86f6385980
kaita <johnkaitadocuments@gmail.com>
2024-02-06 12:35:05 +0300

    Many Corrections

--- web/views.py
+++ web/views.py
@@ -493,8 +493,8 @@ def apply(request):
                                 print(f"Duration is less than 1")
                                 check_against_days = 1
                             else:
-                                print(f"Duration is more than 1 {duration}")
                                 check_against_days = leave.days_in_advance
+                                print(f"Duration is more than 1 {duration} and check against days is {check_against_days}")
                             if duration < check_against_days:
                                 messages.error(request, f"You are supposed to apply at least {leave.days_in_advance} days early. Your application is {application_days_in_advance} days early")
                                 return redirect('apply')
